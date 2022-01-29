5672fbf40cc0e79003476ecc8c7e923f522100e9
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-01-29 19:26:38 +0000

    format

--- gui_win.py
+++ gui_win.py
@@ -3,14 +3,14 @@ print("Launching GUI...")
 import tkinter as tk
 import asyncio
 
+
 def close_windows():
     raise SystemExit
 
+
 async def check_scmd():
     proc = await asyncio.create_subprocess_shell(
-        "where steamcmd",
-        stderr=asyncio.subprocess.PIPE,
-        stdout=asyncio.subprocess.PIPE
+        "where steamcmd", stderr=asyncio.subprocess.PIPE, stdout=asyncio.subprocess.PIPE
     )
 
     stdout, stderr = await proc.communicate()
@@ -27,28 +27,31 @@ async def check_scmd():
 # print(asyncio.run(check_scmd()))
 # if not false, then skip getting scmd
 
+
 class MainWindow:
     def __init__(self, master):
         self.master = master
         self.frame = tk.Frame(self.master, background="black")
         self.master.title("gmodCSSDownloader - Main GUI")
-        #self.master.iconbitmap("icon.ico")
+        # self.master.iconbitmap("icon.ico")
         self.frame.pack()
-        
+
     def create_tips(self):
         self.new_window = tk.TopLevel(self.master)
         self.app = Tips(self.newWindow)
 
+
 class Tips:
-    def __init__(self,master):
+    def __init__(self, master):
         self.master = master
         self.frame = tk.Frame(self.master, background="black")
-        
-  
+
+
 def main():
     root = tk.Tk()
     app = MainWindow(root)
     root.mainloop()
+
 
 # don't check for main, it's going to be called as a module
 # if __name__ == "__main__":
