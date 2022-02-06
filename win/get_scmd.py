0503f69cbf1653ac99c084057f448c74ad77cd40
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-02-06 19:04:12 +0000

    format

--- win/get_scmd.py
+++ win/get_scmd.py
@@ -7,7 +7,8 @@ import os
 
 res = None
 
-def abort(abortcbk,window):
+
+def abort(abortcbk, window):
     if res is not None:
         res.close()
     window.destroy()
@@ -15,19 +16,25 @@ def abort(abortcbk,window):
 
 
 def main(window, frame, successcallback, abortcallback):
-    window.protocol("WM_DELETE_WINDOW", lambda: abort(abortcallback,window))
+    window.protocol("WM_DELETE_WINDOW", lambda: abort(abortcallback, window))
     print("Fetching steamcmd...")
     progvar = tk.StringVar(frame, value="Downloading SteamCMD... (0/0 0%)")
-    proglabel = tk.Label(frame, textvariable=progvar, background="black", foreground="white")
+    proglabel = tk.Label(
+        frame, textvariable=progvar, background="black", foreground="white"
+    )
     proglabel.pack()
-    progress = ttk.Progressbar(frame, orient="horizontal",length=300, mode="determinate")
+    progress = ttk.Progressbar(
+        frame, orient="horizontal", length=300, mode="determinate"
+    )
     progress["value"] = 0
-    progress["maximum"] = 100    
+    progress["maximum"] = 100
     progress.pack()
     window.update()
     dlpath = "./data/steamcmd.zip"
-    with open(dlpath,"wb") as f:
-        res = requests.get("https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip", stream=True)
+    with open(dlpath, "wb") as f:
+        res = requests.get(
+            "https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip", stream=True
+        )
         length = res.headers.get("Content-Length")
         if length is None:
             print("Got no Content-Length header, writing direct to file...")
@@ -39,24 +46,38 @@ def main(window, frame, successcallback, abortcallback):
                 downloaded += len(data)
                 f.write(data)
                 percent_dl = int(100 * (downloaded / length))
-                print("DL Percent: " + str(percent_dl)+"%")
+                print("DL Percent: " + str(percent_dl) + "%")
                 progress["value"] = percent_dl
-                progvar.set("Downloading SteamCMD... ("+str(downloaded)+"/"+str(length)+" "+str(percent_dl)+"%)")
+                progvar.set(
+                    "Downloading SteamCMD... ("
+                    + str(downloaded)
+                    + "/"
+                    + str(length)
+                    + " "
+                    + str(percent_dl)
+                    + "%)"
+                )
                 window.update_idletasks()
     print("Done.")
     print("Extracting...")
     for widget in frame.winfo_children():
         widget.destroy()
-    proglabel = tk.Label(frame, text="Extracting...", background="black", foreground="white")
+    proglabel = tk.Label(
+        frame, text="Extracting...", background="black", foreground="white"
+    )
     proglabel.pack()
-    progress = ttk.Progressbar(frame, orient="horizontal",length=300, mode="indeterminate")
+    progress = ttk.Progressbar(
+        frame, orient="horizontal", length=300, mode="indeterminate"
+    )
     progress["value"] = 0
-    progress["maximum"] = 100    
+    progress["maximum"] = 100
     progress.pack()
     progress.start()
     window.update()
-    with ZipFile("./data/steamcmd.zip", 'r') as zipf:
-        with zipf.open("steamcmd.exe") as srcf, open("./data/steamcmd.exe","wb") as targf:
+    with ZipFile("./data/steamcmd.zip", "r") as zipf:
+        with zipf.open("steamcmd.exe") as srcf, open(
+            "./data/steamcmd.exe", "wb"
+        ) as targf:
             shutil.copyfileobj(srcf, targf)
     os.remove("./data/steamcmd.zip")
     successcallback()
--- win/gui.py
+++ win/gui.py
@@ -6,7 +6,20 @@ import winreg
 import os
 import vdf
 
-regtypeReverseLookup = ["REG_NONE","REG_SZ","REG_EXPAND_SZ","REG_BINARY","REG_DWORD_LITTLE_ENDIAN","REG_DWORD_BIGENDIAN","REG_LINK","REG_MULTI_SZ","REG_RESOURCE_LIST","REG_FULL_RESOURCE_DESCRIPTOR","REG_RESOURCE_REQUIREMENTS_LIST","REG_QWORD_LITTLE_ENDIAN"] # REG_DWORD defaults to REG_DWORD_LITTLE_ENDIAN, REG_QWORD defaults to REG_QWORD_LITTLE_ENDIAN
+regtypeReverseLookup = [
+    "REG_NONE",
+    "REG_SZ",
+    "REG_EXPAND_SZ",
+    "REG_BINARY",
+    "REG_DWORD_LITTLE_ENDIAN",
+    "REG_DWORD_BIGENDIAN",
+    "REG_LINK",
+    "REG_MULTI_SZ",
+    "REG_RESOURCE_LIST",
+    "REG_FULL_RESOURCE_DESCRIPTOR",
+    "REG_RESOURCE_REQUIREMENTS_LIST",
+    "REG_QWORD_LITTLE_ENDIAN",
+]  # REG_DWORD defaults to REG_DWORD_LITTLE_ENDIAN, REG_QWORD defaults to REG_QWORD_LITTLE_ENDIAN
 
 if __name__ == "__main__":
     print("Importing as debug...")
@@ -49,52 +62,95 @@ def find_gmod():
         reg_key = winreg.OpenKey(reg_connection, r"SOFTWARE\\Valve\\Steam")
         reg_value = winreg.QueryValueEx(reg_key, "SteamPath")
         if reg_value[1] != winreg.REG_SZ:
-            print("Invalid type for SteamPath. Expected REG_SZ (int: 1), got "+regtypeReverseLookup[reg_value[1]]+" (int: "+str(reg_value[1])+")")
-            raise OSError("Invalid type for SteamPath. Expected REG_SZ (int: 1), got "+regtypeReverseLookup[reg_value[1]]+" (int: "+str(reg_value[1])+")")
+            print(
+                "Invalid type for SteamPath. Expected REG_SZ (int: 1), got "
+                + regtypeReverseLookup[reg_value[1]]
+                + " (int: "
+                + str(reg_value[1])
+                + ")"
+            )
+            raise OSError(
+                "Invalid type for SteamPath. Expected REG_SZ (int: 1), got "
+                + regtypeReverseLookup[reg_value[1]]
+                + " (int: "
+                + str(reg_value[1])
+                + ")"
+            )
         print(reg_value[0])
-        if os.path.isfile(reg_value[0]+"/steamapps/libraryfolders.vdf"):
+        if os.path.isfile(reg_value[0] + "/steamapps/libraryfolders.vdf"):
             print("Using libraryfolders.vdf...")
             try:
                 # Parse the libraryfolders.vdf file
-                parsed = vdf.parse(open(reg_value[0]+"/steamapps/libraryfolders.vdf", "r"))
+                parsed = vdf.parse(
+                    open(reg_value[0] + "/steamapps/libraryfolders.vdf", "r")
+                )
                 libfolders = parsed["libraryfolders"]
                 del libfolders["contentstatsid"]
                 # Search for Garry's Mod ID
                 for lf in libfolders:
-                    print("Searching in "+libfolders[lf]["path"]+"...")
+                    print("Searching in " + libfolders[lf]["path"] + "...")
                     if "4000" in libfolders[lf]["apps"]:
                         print("Found gmod app ID! Checking path...")
-                        if os.path.isdir(libfolders[lf]["path"]+"\\steamapps\\common\\GarrysMod\\garrysmod"):
+                        if os.path.isdir(
+                            libfolders[lf]["path"]
+                            + "\\steamapps\\common\\GarrysMod\\garrysmod"
+                        ):
                             print("Found Garry's Mod!")
-                            print(libfolders[lf]["path"]+"\\steamapps\\common\\GarrysMod\\garrysmod")
-                            return libfolders[lf]["path"]+"\\steamapps\\common\\GarrysMod\\garrysmod"
+                            print(
+                                libfolders[lf]["path"]
+                                + "\\steamapps\\common\\GarrysMod\\garrysmod"
+                            )
+                            return (
+                                libfolders[lf]["path"]
+                                + "\\steamapps\\common\\GarrysMod\\garrysmod"
+                            )
                         else:
-                            print("Couldn't find Garry's Mod in this library folder despite it being listed in libraryfolders.vdf! Skipping...")
+                            print(
+                                "Couldn't find Garry's Mod in this library folder despite it being listed in libraryfolders.vdf! Skipping..."
+                            )
                 raise FileNotFoundError("Could not find gmod in libraryfolders.vdf!")
-            except (SyntaxError, AttributeError, FileNotFoundError, KeyError, WindowsError) as e:
-                print("Could not parse libraryfolders.vdf\n"+str(e))
-                print("Failed to parse libraryfolders.vdf. Trying steamapps/common where Steam is located...")
+            except (
+                SyntaxError,
+                AttributeError,
+                FileNotFoundError,
+                KeyError,
+                WindowsError,
+            ) as e:
+                print("Could not parse libraryfolders.vdf\n" + str(e))
+                print(
+                    "Failed to parse libraryfolders.vdf. Trying steamapps/common where Steam is located..."
+                )
                 # Use the path to Steam as a guess
-                if os.path.isdir(reg_value[0]+"/steamapps/common/GarrysMod/garrysmod"):
+                if os.path.isdir(
+                    reg_value[0] + "/steamapps/common/GarrysMod/garrysmod"
+                ):
                     print("Found Garry's Mod!")
-                    print(reg_value[0]+"/steamapps/common/GarrysMod/garrysmod")
-                    return reg_value[0]+"/steamapps/common/GarrysMod/garrysmod"
+                    print(reg_value[0] + "/steamapps/common/GarrysMod/garrysmod")
+                    return reg_value[0] + "/steamapps/common/GarrysMod/garrysmod"
                 else:
-                    raise FileNotFoundError("Could not find Garry's Mod in steamapps/common where Steam is located!")
+                    raise FileNotFoundError(
+                        "Could not find Garry's Mod in steamapps/common where Steam is located!"
+                    )
         else:
-            print("Failed to find libraryfolders.vdf. Trying steamapps/common where Steam is located...")
+            print(
+                "Failed to find libraryfolders.vdf. Trying steamapps/common where Steam is located..."
+            )
             # Use the path to Steam as a guess
-            if os.path.isdir(reg_value[0]+"/steamapps/common/GarrysMod/garrysmod"):
+            if os.path.isdir(reg_value[0] + "/steamapps/common/GarrysMod/garrysmod"):
                 print("Found Garry's Mod!")
-                print(reg_value[0]+"/steamapps/common/GarrysMod/garrysmod")
-                return reg_value[0]+"/steamapps/common/GarrysMod/garrysmod"
+                print(reg_value[0] + "/steamapps/common/GarrysMod/garrysmod")
+                return reg_value[0] + "/steamapps/common/GarrysMod/garrysmod"
             else:
-                raise FileNotFoundError("Could not find Garry's Mod in steamapps/common where Steam is located!")
+                raise FileNotFoundError(
+                    "Could not find Garry's Mod in steamapps/common where Steam is located!"
+                )
     except (OSError, WindowsError, EnvironmentError, FileNotFoundError) as e:
-        print("Failed to find/parse Steam data.\n"+str(e))
+        print("Failed to find/parse Steam data.\n" + str(e))
         print("Trying default programfiles (x86) path...")
         # Use the default programfiles (x86) path as a guess
-        if os.path.isdir("c:/program files (x86)/steam/steamapps/common/GarrysMod/garrysmod"):
+        if os.path.isdir(
+            "c:/program files (x86)/steam/steamapps/common/GarrysMod/garrysmod"
+        ):
             print("Found Garry's Mod!")
             print("c:/program files (x86)/steam/steamapps/common/GarrysMod/garrysmod")
             return "c:/program files (x86)/steam/steamapps/common/GarrysMod/garrysmod"
@@ -102,21 +158,46 @@ def find_gmod():
             print("Not found in programfiles (x86), user must enter path manually.")
     return ""
 
+
 def smart_path_select(title, pathvar):
     filename = filedialog.askdirectory(title=title)
     if not str(filename) == "()" and not str(filename) == "":
         pathvar.set(filename)
 
-def start_download(master, frame, gmodpath, assetspath, username, password, successcallback, abortcallback):
+
+def start_download(
+    master,
+    frame,
+    gmodpath,
+    assetspath,
+    username,
+    password,
+    successcallback,
+    abortcallback,
+):
     # TODO: Validate input HERE
     steamcmd_path = asyncio.run(check_scmd())
     if steamcmd_path == False or steamcmd_path == None:
         print("SteamCMD not found!")
-        messagebox.showerror("SteamCMD not found!", "SteamCMD not found! Please restart this program.")
+        messagebox.showerror(
+            "SteamCMD not found!", "SteamCMD not found! Please restart this program."
+        )
         return
-    get_css.main(master, frame, steamcmd_path, gmodpath, assetspath, username, password, successcallback, abortcallback)
+    get_css.main(
+        master,
+        frame,
+        steamcmd_path,
+        gmodpath,
+        assetspath,
+        username,
+        password,
+        successcallback,
+        abortcallback,
+    )
+
 
 steamcmd_path = None
+
 
 class MainWindow:
     def __init__(self, master):
@@ -128,7 +209,7 @@ class MainWindow:
         self.frame.pack()
         self.master.protocol("WM_DELETE_WINDOW", close_windows)
         steamcmd_path = asyncio.run(check_scmd())
-        print("SteamCMD: "+str(steamcmd_path))
+        print("SteamCMD: " + str(steamcmd_path))
         if steamcmd_path == False:
             yn = messagebox.askyesno(
                 "Not found",
@@ -138,31 +219,82 @@ class MainWindow:
                 self.create_scmd_progress(
                     self.scmd_success_callback, self.scmd_abort_callback
                 )
-        self.gmodPathLabel = tk.Label(self.frame, text="garrysmod Path (not the game directory, but the asset directory inside it):", background="black", foreground="white")
+        self.gmodPathLabel = tk.Label(
+            self.frame,
+            text="garrysmod Path (not the game directory, but the asset directory inside it):",
+            background="black",
+            foreground="white",
+        )
         self.gmodPathLabel.grid(row=0, column=0)
         self.gmodPathVar = tk.StringVar(self.frame, value=find_gmod())
-        self.gmodPathEntry = tk.Entry(self.frame, width=65, textvariable=self.gmodPathVar)
+        self.gmodPathEntry = tk.Entry(
+            self.frame, width=65, textvariable=self.gmodPathVar
+        )
         self.gmodPathEntry.grid(row=1, column=0)
-        self.gmodPathButton = tk.Button(self.frame, text="Browse", command=lambda: smart_path_select("Select garrysmod Path (not the game directory, but the asset directory inside it)", self.gmodPathVar))
+        self.gmodPathButton = tk.Button(
+            self.frame,
+            text="Browse",
+            command=lambda: smart_path_select(
+                "Select garrysmod Path (not the game directory, but the asset directory inside it)",
+                self.gmodPathVar,
+            ),
+        )
         self.gmodPathButton.grid(row=1, column=1)
-        self.cssPathLabel = tk.Label(self.frame, text="Asset save path (moving, renaming, deleting etc. will break the assets)", background="black", foreground="white")
+        self.cssPathLabel = tk.Label(
+            self.frame,
+            text="Asset save path (moving, renaming, deleting etc. will break the assets)",
+            background="black",
+            foreground="white",
+        )
         self.cssPathLabel.grid(row=2, column=0)
         self.cssPathVar = tk.StringVar(self.frame, value="./data/cstrike/")
         self.cssPathEntry = tk.Entry(self.frame, width=65, textvariable=self.cssPathVar)
         self.cssPathEntry.grid(row=3, column=0)
-        self.cssPathButton = tk.Button(self.frame, text="Browse", command=lambda: smart_path_select("Select asset save path (moving, renaming, deleting etc. will break the assets)", self.cssPathVar))
+        self.cssPathButton = tk.Button(
+            self.frame,
+            text="Browse",
+            command=lambda: smart_path_select(
+                "Select asset save path (moving, renaming, deleting etc. will break the assets)",
+                self.cssPathVar,
+            ),
+        )
         self.cssPathButton.grid(row=3, column=1)
-        self.loginUserLabel = tk.Label(self.frame, text="Steam Username (it's recommended you leave this as the default)", background="black", foreground="white")
+        self.loginUserLabel = tk.Label(
+            self.frame,
+            text="Steam Username (it's recommended you leave this as the default)",
+            background="black",
+            foreground="white",
+        )
         self.loginUserLabel.grid(row=4, column=0)
         self.loginUserVar = tk.StringVar(self.frame, value="anonymous")
-        self.loginUserEntry = tk.Entry(self.frame, width=65, textvariable=self.loginUserVar)
+        self.loginUserEntry = tk.Entry(
+            self.frame, width=65, textvariable=self.loginUserVar
+        )
         self.loginUserEntry.grid(row=5, column=0)
-        self.loginPassLabel = tk.Label(self.frame, text="Steam Password (it's recommended you leave this as the default)", background="black", foreground="white")
+        self.loginPassLabel = tk.Label(
+            self.frame,
+            text="Steam Password (it's recommended you leave this as the default)",
+            background="black",
+            foreground="white",
+        )
         self.loginPassLabel.grid(row=6, column=0)
         self.loginPassVar = tk.StringVar(self.frame, value="")
-        self.loginPassEntry = tk.Entry(self.frame, width=65, textvariable=self.loginPassVar, show="*")
+        self.loginPassEntry = tk.Entry(
+            self.frame, width=65, textvariable=self.loginPassVar, show="*"
+        )
         self.loginPassEntry.grid(row=7, column=0)
-        self.startButton = tk.Button(self.frame, text="Start Download", command=lambda: self.create_css_progress(self.gmodPathVar.get(), self.cssPathVar.get(), self.loginUserVar.get(), self.loginPassVar.get(), self.css_success_callback, self.css_abort_callback))
+        self.startButton = tk.Button(
+            self.frame,
+            text="Start Download",
+            command=lambda: self.create_css_progress(
+                self.gmodPathVar.get(),
+                self.cssPathVar.get(),
+                self.loginUserVar.get(),
+                self.loginPassVar.get(),
+                self.css_success_callback,
+                self.css_abort_callback,
+            ),
+        )
         self.startButton.grid(row=8, column=0)
 
     def create_scmd_progress(self, successcallback, abortcallback):
@@ -173,18 +305,31 @@ class MainWindow:
     def scmd_success_callback(self):
         self.scmd_window.destroy()
         self.master.deiconify()
-        steamcmd_path = asyncio.run(check_scmd()) # check again after download
-        print("SteamCMD: "+str(steamcmd_path))
-        assert(steamcmd_path != False,"Something has gone terribly wrong in downloading SteamCMD! Please leave an issue on GitHub.")
+        steamcmd_path = asyncio.run(check_scmd())  # check again after download
+        print("SteamCMD: " + str(steamcmd_path))
+        assert (
+            steamcmd_path != False,
+            "Something has gone terribly wrong in downloading SteamCMD! Please leave an issue on GitHub.",
+        )
 
     def scmd_abort_callback(self):
         self.master.deiconify()
         messagebox.showerror("Aborted", "SteamCMD download was aborted!")
-    
-    def create_css_progress(self, gmodpath, assetspath, username, password, successcallback, abortcallback):
+
+    def create_css_progress(
+        self, gmodpath, assetspath, username, password, successcallback, abortcallback
+    ):
         self.master.withdraw()
         self.css_window = tk.Toplevel(self.master)
-        self.app = CSS_Progress(self.css_window, gmodpath, assetspath, username, password, successcallback, abortcallback)
+        self.app = CSS_Progress(
+            self.css_window,
+            gmodpath,
+            assetspath,
+            username,
+            password,
+            successcallback,
+            abortcallback,
+        )
 
     def css_success_callback(self):
         self.master.deiconify()
@@ -204,13 +349,33 @@ class SCMD_Progress:
         self.master.title("gmodCSSDownloader - SteamCMD Progress")
         get_scmd.main(self.master, self.frame, successcallback, abortcallback)
 
+
 class CSS_Progress:
-    def __init__(self, master, gmodpath, assetspath, username, password, successcallback, abortcallback):
+    def __init__(
+        self,
+        master,
+        gmodpath,
+        assetspath,
+        username,
+        password,
+        successcallback,
+        abortcallback,
+    ):
         self.master = master
         self.frame = tk.Frame(self.master, background="black")
         self.frame.pack()
         self.master.title("gmodCSSDownloader - CS:S Progress")
-        start_download(self.master, self.frame, gmodpath, assetspath, username, password, successcallback, abortcallback)
+        start_download(
+            self.master,
+            self.frame,
+            gmodpath,
+            assetspath,
+            username,
+            password,
+            successcallback,
+            abortcallback,
+        )
+
 
 def main():
     print("Launching GUI...")
