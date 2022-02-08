63159bf64d9c8d65372630bfeae6566be7951252
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-02-08 21:45:43 +0000

    builds are now repeatable

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
-pyinstaller --onefile main.py --specpath ./spec/ --name=gmodCSSDownloader --icon=icon.ico
+# set seed to a known repeatable integer value
+PYTHONHASHSEED=1
+export PYTHONHASHSEED
+SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
+export SOURCE_DATE_EPOCH
+# create one-file build as myscript
+pyinstaller ./spec/gmodCSSDownloader.spec
+# make checksum
+cksum dist/gmodCSSDownloader | awk '{print $1}' > dist/checksum.txt
+# let Python be unpredictable again
+unset PYTHONHASHSEED
